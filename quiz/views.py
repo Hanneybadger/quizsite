a2fa4a51e0a047ba85a2e4fb2dbd07208dfead08
Hanneybadger <hwillen@gmail.com>
2015-08-06 10:10:31 +0200

    skapat 4 vyer i quiz/views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+	return render(request, "quiz/quizmestart.html")
+
+def quiz(request):
+	return render(request, "quiz/quizmedog.html")
+
+def question(request):
+	return render(request, "quiz/quizmedogquestions.html")
+
+def completed(request):
+	return render(request, "quiz/quizmedogresults.html")
