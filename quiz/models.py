d4ba6f6b18199a141b7530a277b8a69eeeacfa79
Hanneybadger <hwillen@gmail.com>
2015-08-07 09:41:49 +0200

    Databastut torsdag klar

--- quiz/models.py
+++ quiz/models.py
@@ -6,8 +6,11 @@ class Quiz(models.Model):
 	slug = models.SlugField(max_length=150)
 	description = models.TextField()
 
+	def __unicode__(self):
+		return self.name
+
 class Question(models.Model):
-	quiz = models.ForeignKey(Quiz)
+	quiz = models.ForeignKey(Quiz, related_name="questions")
 	question = models.TextField()
 	answer1 = models.CharField(max_length=250)
 	answer2 = models.CharField(max_length=250)
@@ -15,4 +18,6 @@ class Question(models.Model):
 	answer4 = models.CharField(max_length=250)
 	correct = models.PositiveIntegerField()
 
-	
+	def __unicode__(self):
+		return self.quiz.name + "/" + self.question
+
