6e6e100309e1c7619442b9fc451d4b6fd0599f24
Hanneybadger <hwillen@gmail.com>
2015-08-07 10:18:51 +0200

    lagt in så html-sidorna och views hämtar från min django-databas
    Nästa steg är “Nu ska vi se till att det svar som användaren väljer
    postas till rätt sida:”

--- quiz/views.py
+++ quiz/views.py
@@ -1,44 +1,57 @@
 # coding: utf-8
+from quiz.models import Quiz
 
 from django.shortcuts import render
 
-quizzes = {
-	"klassiker": {
-   		"name": u"Klassiska böcker",
-	   	"description": u"Hur bra kan du dina klassiker?"
-	},
-	"fotboll": {
-	   	"name": u"Största fotbollslagen",
-	   	"description": u"Kan du dina lag?"
-	},
-	"kanda-hackare": {
-	    	"name": u"Världens mest kända hackare",
-	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
-}
+from django.shortcuts import redirect
+
+
+
+# quizzes = {
+# 	"klassiker": {
+#    		"name": u"Klassiska böcker",
+# 	   	"description": u"Hur bra kan du dina klassiker?"
+# 	},
+# 	"fotboll": {
+# 	   	"name": u"Största fotbollslagen",
+# 	   	"description": u"Kan du dina lag?"
+# 	},
+# 	"kanda-hackare": {
+# 	    	"name": u"Världens mest kända hackare",
+# 	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+# }
 
 
 # Create your views here.
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/quizmestart.html", context)
 
 def quiz(request, slug):
 	context = {
-		"quiz": quizzes[slug],
-		"quiz_slug": slug,
+		"quiz": Quiz.objects.get(slug=slug),
+#		"quiz_slug": slug,
 	}
 	return render(request, "quiz/quizmedog.html", context)
 
 def question(request, slug, number):
+	number = int(number)
+	quiz = Quiz.objects.get(slug=slug)
+	questions = quiz.questions.all()
+	if number > questions.count():
+		return redirect("completed_page", quiz.slug)
+	question = question = questions[number - 1]
+	
 	context = {
 		"question_number": number,
-		"question": u"hur många multar har ölandsbron?",
-		"answer1": u"12",
-		"answer2": u"66 400",
-		"answer3": u"7 428 954",
-		"quiz_slug": slug,
+		"question": question.question,
+		"answer1": question.answer1,
+		"answer2": question.answer2,
+		"answer3": question.answer3,
+		"answer4": question.answer4,
+		"quiz": quiz,
 	}
 
 	return render(request, "quiz/quizmedogquestions.html", context)
